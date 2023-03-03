ae1b97824664902bf02a9c795bc995e03a8d795d
Taahir Bhaiyat <taahir.bhaiyat@camcog.com>
2023-03-03 16:42:12 +0200

    Check server requests and responses

--- socket.test.js
+++ socket.test.js
@@ -26,16 +26,38 @@ test("that chat app can be mocked", (done) => {
     socket.on("message", (message) => {
       // Whenever the server receives a message
       console.log("RECEIVED MESSAGE FROM CLIENT");
-      if (message) {
-        const type = JSON.parse(message).type;
-        switch (type) {
-          case "getTestResourceChanges":
-            socket.send(message); // We just send the same message back to the client (for now)
-            break;
-          default:
-            socket.send(null);
-        }
-      }
+      
+      // Check if the message structure is ok
+      message = JSON.parse(message);
+      expect(message).toBeTruthy();
+      
+      expect(message.resources).toBeDefined(); 
+      expect(typeof message.resources).toBe("object"); 
+      
+      expect(message.requestId).toBeDefined();
+      expect(typeof message.requestId).toBe("number");
+      
+      expect(message.type).toBeDefined();
+      expect(message.type).toBe("getTestResourceChanges");
+      
+      // Socket sends a series of messages
+      message["type"] = "testResourceUpdate";
+      message["uri"] = "speech/en-US/hello.mp3";
+      message["version"] = "abc123";
+      socket.send(JSON.stringify(message));
+
+      message["type"] = "testResourceDelete";
+      delete message.version;
+      socket.send(JSON.stringify(message));
+
+      message["type"] = "testResourceTotal";
+      message["n"] = 207;
+      socket.send(JSON.stringify(message));
+
+      message["type"] = "testResourcesCompleted";
+      delete message.n;
+      socket.send(JSON.stringify(message));
+
     });
   });
 
@@ -47,14 +69,25 @@ test("that chat app can be mocked", (done) => {
   };
   app.sendMessage(JSON.stringify(getTestResourceChangesMsg));
 
-
   setTimeout(() => {
-    const messages = app.messages.map(msg => JSON.parse(msg)); 
-    console.log({messages});
+    const messages = app.messages.map((msg) => JSON.parse(msg));
+    console.log({ messages });
 
-    expect(messages.length).toBe(1)
-    expect(messages[0].type).toBe("getTestResourceChanges");
-    
+    expect(messages.length).toBe(4);
+    messages.forEach((msg) => {
+      expect(msg.type).toBeTruthy(); 
+      switch (msg.type) {
+        case "testResourceUpdate":
+          break;
+        case "testResourceDelete":
+          break;
+        case "testResourceTotal":
+          break;
+        case "testResourcesCompleted":
+          break;
+      }
+    });
+
     mockServer.stop(done);
   }, 100);
 });
